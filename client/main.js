149e63596347549e817b68dd3ea9be36c71a39c8
keighty <k80.leonard@gmail.com>
2016-12-17 11:12:00 -0800

    Add user accounts.

--- client/main.js
+++ client/main.js
@@ -1,3 +1,4 @@
 import '../imports/ui/body.js'
 import '../imports/ui/task.html'
 import '../imports/api/tasks.js'
+import '../imports/startup/accounts-config.js'
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -30,6 +30,8 @@ Template.body.events({
     Tasks.insert({
       text,
       createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
     })
 
     target.text.value = ''
