046044370d09691872c2b7df9a294bbd55bd754e
keighty <k80.leonard@gmail.com>
2016-12-17 10:34:24 -0800

    Add basic todo app.

---
+++ imports/ui/body.js
@@ -0,0 +1,24 @@
+import { Template } from 'meteor/templating'
+import { Tasks } from '../api/tasks.js'
+import './body.html'
+
+Template.body.helpers({
+  tasks() {
+    return Tasks.find({})
+  },
+})
+
+Template.body.events({
+  'submit .new-task'(e) {
+    e.preventDefault()
+    const target = e.target
+    const text = target.text.value
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    })
+
+    target.text.value = ''
+  }
+})
